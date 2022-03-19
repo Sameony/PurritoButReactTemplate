76015a7a9005162996bb4ffc9937e4fef6bd03b8
Sameony <iam2001sam@gmail.com>
2022-03-19 20:36:27 +0530

    feat: mock product data

---
+++ src/Data/cart.js
@@ -0,0 +1,52 @@
+const prod = [
+    {
+        id: 1,
+        imageUrl: "Images/kyotPeek.jpg",
+        isLiked: false,
+        title: "kyotPeek",
+        color: "white",
+        price: 18000,
+        rating: "4.4",
+        category: "blanket"
+    },
+    {
+        id: 2,
+        imageUrl: "Images/kyotPeek.jpg",
+        isLiked: false,
+        title: "kyotPeek",
+        color: "white",
+        price: 18000,
+        rating: "4.4",
+        category: "blanket"
+    },
+    {
+        id: 3,
+        imageUrl: "Images/kyotComfy.jpg",
+        isLiked: true,
+        title: "kyotComfy",
+        color: "white",
+        price: 18000,
+        rating: "4.4",
+        category: "blanket"
+    },
+    {
+        id: 4,
+        imageUrl: "Images/kyotComfy.jpg",
+        isLiked: false,
+        title: "kyotComfy",
+        color: "white",
+        price: 18000,
+        rating: "4.4",
+        category: "blanket"
+    },
+    {
+        id: 5,
+        imageUrl: "Images/kyotPeek.jpg",
+        isLiked: false,
+        title: "kyotPeek",
+        color: "white",
+        price: 18000,
+        rating: "4.4",
+        category: "blanket"
+    }
+]
