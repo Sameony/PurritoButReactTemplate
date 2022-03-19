5cbf007d3c064e810f4ef5a28451d1d1bb0d709f
Sameony <iam2001sam@gmail.com>
2022-03-19 22:45:48 +0530

    Initial commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,7 @@
+import Landing from "./Landing";
+import Cart from "./cart";
+import Wishlist from "./wishlist";
+import Products from "./products";
+import Invalid from "./invalid";
+
+export {Cart, Landing, Wishlist, Products, Invalid};
